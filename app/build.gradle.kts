85e00e18a5d53878c979cba181a8d8857d4aed40
Iyannn <rianandri1575@gmail.com>
2026-09-15 16:08:08 +0700

    fix(build): remove duplicate kotlin.android plugin conflicting with AGP built-in Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ import com.google.gms.googleservices.GoogleServicesPlugin.MissingGoogleServicesS
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
-  alias(libs.plugins.kotlin.android)
   alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
   alias(libs.plugins.secrets)
